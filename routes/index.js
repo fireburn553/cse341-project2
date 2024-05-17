ab2c95f34fedd6e576b1687693349860dc3644b8
James Charlie I. Salva <56073573+fireburn553@users.noreply.github.com>
2024-05-17 22:02:25 +0800

    added swagger to the file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,12 @@
 const router = require("express").Router();
 
 router.get("/", (req, res) => {
+  //#swagger.tags=['Hello World']
   res.send("Hello, world!");
 });
 
 router.use("/students", require("./students"));
+
+router.use("/", require("./swagger"));
 
 module.exports = router;
